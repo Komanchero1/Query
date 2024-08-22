82866f9cb07d1b809239233e0cec3dafbcd78905
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-08-22 12:23:33 +0400

    Delete src/main/java/ServerHahdler directory

--- src/main/java/ServerHahdler/Handler.java
+++
@@ -1,9 +0,0 @@
-package ServerHahdler;
-
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-
-interface Handler {
-    void handle(Request request, BufferedOutputStream responseStream) throws IOException;
-}
